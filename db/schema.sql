608ebbf1d0c489482c2e14c212060a7b32a7887a
marioarau <49352834+marioarau@users.noreply.github.com>
2019-11-19 08:32:41 -0800

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS nonprofit2;
+CREATE DATABASE nonprofit2;
+
---
+++ db/seed_categories.sql
@@ -0,0 +1,10 @@
+
+insert into Categories (name,createdAt,updatedAt) values ('Domestic Violence Support Services',now(),now());
+insert into Categories (name,createdAt,updatedAt) values ('AIDS/HIV Services',now(),now());
+insert into Categories (name,createdAt,updatedAt) values ('Children and Youth',now(),now());
+insert into Categories (name,createdAt,updatedAt) values ('Community Organizing / Advocacy',now(),now());
+insert into Categories (name,createdAt,updatedAt) values ('Human Rights and Civil Liberties',now(),now());
+insert into Categories (name,createdAt,updatedAt) values ('Race and Ethnicity',now(),now());
+insert into Categories (name,createdAt,updatedAt) values ('Voting',now(),now());
+insert into Categories (name,createdAt,updatedAt) values ('Youth Services',now(),now());
+
